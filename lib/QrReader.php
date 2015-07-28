59d4f18025c31d9d5149488f76168f7b77b47041
Karthik Sekar <kaysekar@gmail.com>
2015-07-28 12:12:26 +1200

    separate function to get a luminance source is added

--- lib/QrReader.php
+++ lib/QrReader.php
@@ -11,24 +11,29 @@ abstract class QrReader
 {
     public $result;
 
-    public function decode($filename)
+    public function getLuminanceSource($imageFileName)
+    {
+        if (extension_loaded('imagick')) {
+            $im = new \Imagick();
+            $im->readImage($imageFileName);
+            $width = $im->getImageWidth();
+            $height = $im->getImageHeight();
+
+            return new IMagickLuminanceSource($im, $width, $height);
+        }
+
+        $image = file_get_contents($imageFileName);
+        $sizes = getimagesize($imageFileName);
+        $width = $sizes[0];
+        $height = $sizes[1];
+        $im = imagecreatefromstring($image);
+
+        return new GDLuminanceSource($im, $width, $height);
+    }
+
+    public function decode(LuminanceSource $source)
     {
         try {
-            if (extension_loaded('imagick')) {
-                $im = new \Imagick();
-                $im->readImage($filename);
-                $width = $im->getImageWidth();
-                $height = $im->getImageHeight();
-                $source = new IMagickLuminanceSource($im, $width, $height);
-            } else {
-                $image = file_get_contents($filename);
-                $sizes = getimagesize($filename);
-                $width = $sizes[0];
-                $height = $sizes[1];
-                $im = imagecreatefromstring($image);
-
-                $source = new GDLuminanceSource($im, $width, $height);
-            }
             $histo = new HybridBinarizer($source);
             $bitmap = new BinaryBitmap($histo);
             $reader = new QRCodeReader();
